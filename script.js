42842279e4a5b0117d40fac6366144e8b75421c5
Ahmed Khalid <ahmed.k.m12@hotmail.com>
2025-06-05 01:44:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -40,15 +40,7 @@ btnScroll.addEventListener('click', function () {
   const scrollDownTo = section1.getBoundingClientRect();
   console.log(scrollDownTo);
   console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset); //the scroll posetion
-  // console.log(
-  //   'height/width viewport',
-  //   document.documentElement.clientHeight,
-  //   document.documentElement.clientWidth
-  // );
-  // window.scrollTo({
-  //   left: window.pageXOffset + scrollDownTo.left,
-  //   top: window.pageYOffset + scrollDownTo.top,
-  //   behavior: 'smooth',
+
   // });//the old way may used some time
   section1.scrollIntoView({ behavior: 'smooth' });
 });
@@ -210,15 +202,7 @@ slider();
 //   });
 // });
 
-////////the old scoll of making the page navigation
-// document.querySelectorAll('.nav__link').forEach(function (el) {
-//   el.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute('href');
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-//   });
-// });
+
 //the new way of implemeting the navigation
 document.querySelector('.nav__links').addEventListener('click', function (el) {
   el.preventDefault();
@@ -227,35 +211,7 @@ document.querySelector('.nav__links').addEventListener('click', function (el) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
-//implementing the hover over the links
-//1 add the not hover to all the nodes tha have class called not hoverd
-//2 add the hover effect to the the target
-//implementing the hover effevt in the bars link
-//bad bractice way
-// nav.addEventListener('mouseover', function (e) {
-//   if (e.target.classList.contains('nav__link')) {
-//     const link = e.target;
-//     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-//     const logo = link.closest('.nav').querySelector('img');
-//     siblings.forEach(el => {
-//       if (el !== link) el.style.opacity = 0.5;
-//       console.log(el.style.opacity);
-//     });
-//     logo.style.opacity = 0.5;
-//   }
-// });
-// nav.addEventListener('mouseout', function (e) {
-//   if (e.target.classList.contains('nav__link')) {
-//     const link = e.target;
-//     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-//     const logo = link.closest('.nav').querySelector('img');
-//     siblings.forEach(el => {
-//       if (el !== link) el.style.opacity = 1;
-//       console.log(el.style.opacity);
-//     });
-//     logo.style.opacity = 1;
-//   }
-// });
+
 const handler = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
@@ -271,95 +227,9 @@ const handler = function (e) {
 nav.addEventListener('mouseover', handler.bind(0.5));
 nav.addEventListener('mouseout', handler.bind(1));
 
-// Window.addEventListener('beforeunload', function (e) {
-//   e.preventDefault();
-//   e.returnValue = 'message';
-// });
-//implementing the sticky position
-////////////////////////////////
-///////////////////////////////
-//the end of the project
-//get elementbytagname and the by classnam,e return html collection not as the queryselectorAll return node list
-// console.log(document.getElementsByTagName('button'));
-// //creating element and remove insert
-// //creating element
-// const message = document.createElement('div');
-// const header = document.querySelector('.header');
-// message.classList.add('cookie-message');
-// message.innerHTML =
-//   'hello my name us ahmed and u an web dwvwelpwe<button class="btn btn--close-cookie">Got it!</button>';
-// header.prepend(message);
-// //deleat element
-// document
-//   .querySelector('.btn--close-cookie')
-//   .addEventListener('click', function () {
-//     // message.remove();
-//     // message.parentElement.removeChild(message);
-//     // message.remove();
-//     message.parentElement.removeChild(message); //called node traveersale doesnot user anr more use instead .remove
-//   });
-//style and attribute
-// message.style.background = 'orangered';
-// console.log(getComputedStyle(message).height);
-// message.style.height = parseFloat(getComputedStyle(message).height) + 30 + 'px';
-// //custsemaize elements variable in the root and the root is the document in javasc(properrty)
-// document.documentElement.style.setProperty('--color-primary', 'blue');
-// //attribute in action
-// const logo = document.querySelector('.nav__logo');
-// console.log(logo.alt);
-// console.log(logo.src);
-// console.log(logo.className);
-// //non stsndard
-// logo.setAttribute('developer', 'ahmed');
-// console.log(logo.getAttribute('developer'));
 
-// console.log(logo.getAttribute('src')); //get the realative path
-// //data attribute
-// console.log(logo.dataset.versionNumber);
-// //classes
-// logo.classList().add('any class');
-// logo.classList().remove('any class');
-// logo.classList().toggle('any class');
-//(new) types of event and event handler
-// document.querySelector('h1').addEventListener('mouseenter', function () {
-//   document.querySelector('body').style.color = 'red';
-//   ocument.querySelector('h1').r
-// });
-// const h1 = document.querySelector('h1');
-// const alert = function (e) {
-//   h1.style.color = 'red';
-// };
-// h1.addEventListener('mouseenter', alert);
-// setTimeout(() => {
-//   h1.removeEventListener('mouseenter', alert);
-// }, 3000);
-// //implemetn the randome color
-// const random = function (min, max) {
-//   return Math.floor(Math.random() * max - min) + min;
-// };
-// console.log(random(10, 20));
-// const randomColor = function () {
-//   return `rgb(${(random(0, 255), random(0, 255), random(0, 255))})`;
-// };
-// document.querySelector('.nav__link').addEventListener('click', function (e) {
-//   this.style.color = randomColor();
-// });
-//event propgation in practice
-//
+
 
 //event delegation
 const h1 = document.querySelector('h1');
 
-// Going downwards: child
-// console.log(h1.querySelectorAll('.highlight'));
-// console.log('child nodes', h1.childNodes);//
-// console.log('childer', h1.children);
-// h1.firstElementChild.style.color = 'white';
-// h1.lastElementChild.style.color = 'orangered';
-// Going upwards: parents
-// console.log(h1.parentNode);
-// console.log(h1.parentElement);
-
-// h1.closest('.header').style.background = 'var(--gradient-secondary)';
-
-// h1.closest('h1').style.background = 'var(--gradient-primary)';
